744812591317cf1f84cc56c2f462ceb787aaaba3
zisnir1@gmail.com <zisnir1@gmail.com>
2026-01-04 18:09:18 +0000

    new model

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -3,9 +3,8 @@ with cte as
     select to_timestamp(START_AT) as started_at
         ,date(started_at) as date_started_at
         ,hour(started_at) as hour_started_at
-        , {{day_type('start_at')}} as day_type
+        , {{day_type('start_at')}} as day_type --macro
         , {{get_season('start_at')}} as season --using macros from date_utils.sql
-
     from {{ source('demo', 'bike') }}
     where START_AT <> 'started_at'
 )
