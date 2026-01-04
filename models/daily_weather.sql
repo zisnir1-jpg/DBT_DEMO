6b3f7d5d60701c9a9b868ce4659295238bda3f20
zisnir1@gmail.com <zisnir1@gmail.com>
2026-01-04 18:06:23 +0000

    daily_weather model created

---
+++ models/daily_weather.sql
@@ -0,0 +1,26 @@
+with daily_weather as
+(
+    select date(time) as daily_weather
+        ,weather
+        ,temp
+        ,pressure
+        ,humidity
+        ,clouds
+    from {{ source('demo', 'weather') }}
+)
+
+, daily_weather_agg as
+(
+    select daily_weather
+        ,weather
+        ,round(avg(temp), 2) as avg_temp
+        ,round(avg(pressure), 2) as avg_pressure
+        ,round(avg(humidity),2 ) as avg_humidity
+        ,round(avg(clouds), 2) as avg_clouds
+        from daily_weather
+        group by all
+        qualify row_number() over(partition by daily_weather order by count(weather) desc) = 1
+)
+
+select *
+from daily_weather_agg
--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -3,12 +3,9 @@ with cte as
     select to_timestamp(START_AT) as started_at
         ,date(started_at) as date_started_at
         ,hour(started_at) as hour_started_at
-        ,case when dayname(started_at) in ('Sat', 'Sun') then 'WEEKEND'
-            ELSE 'BUSINESSDAY' END AS DAY_TYPE
-        ,CASE WHEN MONTH(started_at)  IN (12,1,2) THEN 'WINTER'
-            WHEN MONTH(started_at) IN (3,4,5) THEN 'SPRING'
-            WHEN MONTH(started_at) IN (6,7,8) THEN 'SUMMER'
-            ELSE 'AUTUMN' END AS STATION_OF_YEAR
+        , {{day_type('start_at')}} as day_type
+        , {{get_season('start_at')}} as season --using macros from date_utils.sql
+
     from {{ source('demo', 'bike') }}
     where START_AT <> 'started_at'
 )
